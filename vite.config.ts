c26df87490c8124a10f9fad89d7cbad9f58d5bd8
EstebanCaso <eecaso04@gmail.com>
2025-06-24 20:23:31 -0700

    Fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Insoft/', 
   plugins: [react()],
-  base: '/insoft/', 
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
